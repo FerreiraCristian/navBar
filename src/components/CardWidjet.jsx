d144eb74feaa1f4e2babf4cd7a08d2c5477cd0bd
cris2694 <cris2694@gmail.com>
2022-07-20 23:03:19 -0300

    segundoCommit

---
+++ src/components/CardWidjet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa';
+
+
+
+FaShoppingCart
+const CardWidjet = () => {
+    return (
+        <FaShoppingCart/>
+    );
+}
+
+export default CardWidjet;
